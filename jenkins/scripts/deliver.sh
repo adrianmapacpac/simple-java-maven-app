7fc13d00d9d48d6be2d1d5ecb6966c2a31c338e0
adrianmapacpac <adrian.mapacpac@gmail.com>
2024-04-29 23:57:44 -0400

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-MAVEN_OPTS=-Dfile.encoding=UTF-8
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
